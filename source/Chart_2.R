6c5c807761fe7a2a66d340796a9bed9d6ed830d0
Zohaib <zohaibj10@gmail.com>
2022-02-18 16:51:56 -0800

    add chart 2

---
+++ source/Chart_2.R
@@ -0,0 +1,27 @@
+library("ggplot2")
+library("dplyr")
+library("stringr")
+library("tidyverse")
+
+owid_data_csv <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-aakash-krishna/main/data/owid-co2-data.csv")
+continents <- c("Asia", "Africa", "North America", "South America", "Europe", "Australia")
+
+by_country_df <- owid_data_csv %>%
+  filter(population > 128800000) %>%
+  filter(country %in% continents != TRUE) %>%
+  filter(country != "World") %>%
+  filter(year > 1950) %>%
+  mutate(country = fct_reorder2(country, year, co2)) %>%
+  group_by(country) %>%
+
+  select(country, year, co2)
+
+ggplot(data = by_country_df) +
+  geom_line(
+    mapping = aes(x = year, y = co2, color = country), size = 1.5)  +
+  ggtitle("CO2 Emissions of Highly Populated Countries (100m+) by Year") +
+  ylab("CO2 Emissions (in million metric tons)") +
+  xlab("Year")
+
+
+View(by_country_df)
