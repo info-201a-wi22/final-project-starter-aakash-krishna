b0b1f16cd9bc8e8833b82138db80b000af1726da
ajaypai <ajaypai@cs.washington.edu>
2022-02-18 21:49:54 -0800

    power plant summary info

---
+++ source/Summary_Info.R
@@ -0,0 +1,17 @@
+#dataset
+power_plant_data <- read.csv("../data/global_power_plant_database_last.csv") %>%
+  filter(primary_fuel != "Waste" & primary_fuel != "Storage" & 
+           primary_fuel != "Petcoke" & primary_fuel != "Cogeneration" &
+           primary_fuel != "Biomass")
+
+#summary info
+energy_source_with_most_number_of_power_planst <- power_plant_data %>% 
+  group_by(primary_fuel) %>%
+  tally() %>% 
+  filter(n == max(n)) %>% 
+  pull(primary_fuel)
+most_used_energy_source <- power_plant_data %>% 
+  group_by(primary_fuel) %>% 
+  summarise(total_capacity = sum(capacity_mw)) %>% 
+  filter(total_capacity == max(total_capacity)) %>% 
+  pull(primary_fuel)
