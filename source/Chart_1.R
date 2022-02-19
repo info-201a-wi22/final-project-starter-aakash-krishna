2e0377cb7f6d13a48ecfe90feacb1de82c789fe0
Aakash <aakash_krishna@outlook.com>
2022-02-18 19:43:08 -0800

    quality of life changes to chart 1

--- source/Chart_1.R
+++ source/Chart_1.R
@@ -7,19 +7,19 @@ setwd(".")
 consumption_data <- read.csv("../data/Primary-energy-consumption-from-fossilfuels-nuclear-renewables.csv")
 
 data <- consumption_data %>%
-  filter(Entity == "Bangladesh" | Entity == "Brazil" | Entity == "China" |
+  filter(Entity == "Brazil" | Entity == "China" |
            Entity == "India" | Entity == "Mexico" |
            Entity == "Nigeria" |Entity == "Pakistan" |Entity == "Russia" |
            Entity == "United States") %>%
   select("Entity", "Year", "Renewables....sub.energy.", 
          "Nuclear....sub.energy.") %>%
-  mutate(ratio = ifelse(Renewables....sub.energy. == 0, 0, Nuclear....sub.energy./Renewables....sub.energy.)) %>%
-  mutate(text = paste0("Country: ", Entity, "\nYear: ", Year, "\nRatio of nuclear energy to other renewable energies used: ", ratio))
+  mutate(ratio = ifelse(Renewables....sub.energy. == 0, 0, Nuclear....sub.energy./Renewables....sub.energy.))
+  
 
 tabl <- data %>% 
   ggplot(aes(x = Year, y = Nuclear....sub.energy., color = Entity, size = ratio)) + 
   geom_point(alpha=0.7) +
-  scale_size(range = c(1.4, 15), name="Nuclear/Renewable Ratio") +
+  scale_size(range = c(1.2, 16.5), name="Nuclear/Renewable Ratio") +
   ggtitle("Changes in Usage of Nuclear Energy") + 
   xlab("Year") +
   ylab("Percent of Energy from Nuclear") +
